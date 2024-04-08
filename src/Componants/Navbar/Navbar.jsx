1c0ee60aa8a04d4af9a8a8674fdf2d1c29887eed
bipinsinhrathod <bipinrajput548@gmail.com>
2024-04-08 14:28:41 +0530

    FirstCommit

--- src/Componants/Navbar/Navbar.jsx
+++ src/Componants/Navbar/Navbar.jsx
@@ -12,12 +12,12 @@ const Navbar = () => {
     <>
       <section className=" flex justify-between bg-[#001529] ">
         <div className="flex items-center  ">
-          <input className="rounded-lg ml-10 p-2 bg-[#334454]" type="text" />
+          <input className="rounded-lg ml-10 text-white outline-none p-2 bg-[#334454]" type="text" />
         </div>
 
         <div className="flex mr-10 space-x-8 p-5">
           <button
-            className={` text-gray-300 p-2 rounded-md hover:text-white duration-300 transition-all ${
+            className={` text-gray-300 p-2 rounded-md hover:text-white duration-300 transition-all hover:scale-110 ${
               activeBtn === 'home' ? 'bg-[#1677FF]' : "bg-[#001529]"
             } `}
             onClick={() => handleClick('home')}
@@ -25,7 +25,7 @@ const Navbar = () => {
             Home
           </button>
           <button
-            className={` text-gray-300 p-2 rounded-md hover:text-white duration-300 transition-all ${
+            className={` text-gray-300 p-2 rounded-md hover:text-white duration-300 transition-all hover:scale-110 ${
               activeBtn === 'about' ? 'bg-[#1677FF]' : "bg-[#001529]"
             } `}
             onClick={() => handleClick('about')}
@@ -33,7 +33,7 @@ const Navbar = () => {
             About
           </button>
           <button
-            className={` text-gray-300 p-2 rounded-md hover:text-white duration-300 transition-all ${
+            className={` text-gray-300 p-2 rounded-md hover:text-white duration-300 transition-all hover:scale-110 ${
               activeBtn === 'contact' ? 'bg-[#1677FF]' : "bg-[#001529]"
             } `}
             onClick={() => handleClick('contact')}
